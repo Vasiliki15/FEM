c9cace594ed5d9e3765865ceeb65579fe7dcf288
Vasiliki Skouroliakou <vasiaskr15@gmail.com>
2020-02-02 18:46:23 +0200

    Change the localstiffness function
    Finds a zero det at a combination of nodes so you can't reverse, you
    nned to use Gauss elimination

--- fem.c
+++ fem.c
@@ -8,21 +8,17 @@ enum boundary_cond {inner_node,Dirichlet};
 
 
 int find_det(const int array_b[][2]);
-void local_stiffness(const int array_b[][2], int inv_array_b[][2], float A_local[][3]);
+void local_stiffness(int det, int inv_array_b[][2], float A_local[][3]);
 void find_inv_matrix(const int array_b[][2], int inv_array_b[][2]);
-/*void construct_b(int x1, int x2, int x3);*/
+void construct_b(int x[3], int y[3], int array_b[][2]);
+void construct_global_matrix(float A_global[][N]);
 
 
 int main(){
 	enum boundary_cond cond_d[21];
-	int i,j,det;
+	int i;
 	float A_global[N][N]={0};
-	float A_local[3][3];
-	float *local_array;
 	float b[N]={0};
-	int array_b[2][2]={{2,1},{3,2}};
-	int inv_array_b[2][2];
-	
 
 /* mark nodes on the boundary,to impose Dirichlet boundary condition */
 	for (i=0; i<N; i++){
@@ -33,14 +29,16 @@ int main(){
 			cond_d[i]=inner_node;
 		}
 	}
-	/*local=&A_local[0][1];*/
-	find_inv_matrix(array_b, inv_array_b);
-	local_stiffness(array_b,inv_array_b,A_local);
-	for (i=0; i<3; i++){
-		for(j=0; j<3; j++){
-			printf("%f\n", A_local[i][j]);
+
+	 
+	
+	
+	construct_global_matrix(A_global);
+	/*for (i=0; i<2; i++){
+		for(j=0; j<2; j++){
+			printf("%d\n", array_b[i][j]);
 		}
-	}
+	}*/
 	return 0;
 }
 
@@ -57,19 +55,21 @@ void find_inv_matrix(const int array_b[][2], int inv_array_b[][2]){
 	int det;
 	
 	det=find_det(array_b);
-
+	if (det==0){
+		printf("u r fuckkkkkkked\n");
+	}
 	inv_array_b[0][0] = array_b[1][1]/det;
 	inv_array_b[0][1] = -array_b[1][0]/det;
 	inv_array_b[1][0] = -array_b[0][1]/det;
 	inv_array_b[1][1] = array_b[0][0]/det;
 }
 
-void local_stiffness(const int array_b[][2], int inv_array_b[][2],float A_local[][3]) {
-	int det,i,j;
+void local_stiffness(int det, int inv_array_b[][2],float A_local[][3]) {
+	int i,j;
 	float area=0.5;
 	int phib[2][3]={ {inv_array_b[0][0], inv_array_b[0][1], -inv_array_b[0][0]-inv_array_b[0][1]}, {inv_array_b[1][0], inv_array_b[1][1], -inv_array_b[1][0]-inv_array_b[1][1]} };
 	
-	det=find_det(array_b);
+	
 	for(i=0; i<3; i++){
 		for(j=0;j<3;j++){
 			A_local[i][j]= area*det*(phib[0][i]*phib[0][j] + phib[1][i]*phib[1][j]);
@@ -77,6 +77,36 @@ void local_stiffness(const int array_b[][2], int inv_array_b[][2],float A_local[
 	}
 }
 
-/*void construct_b(int x1, int x2, int x3){*/
+void construct_b(int x[3], int y[3], int array_b[][2]){
 
+	array_b[0][0]=x[0]-x[2];
+	array_b[0][1]=y[0]-y[2];
+	array_b[1][0]=x[1]-x[2];
+	array_b[1][1]=y[1]-y[2];
+}
+void construct_global_matrix(float A_global[][N]){
+	int i,j,sel,det,k,l;
+	int arr_x[3];
+	int arr_y[3];
+	int array_b[2][2], inv_array_b[2][2];
+	float A_local[3][3]={0};
+
+	for (i=0; i<NT; i++){
+		for (j=0; j<3; j++){
+			sel=elem[i][j];
+			arr_x[j]=nodes[sel][0];
+			arr_y[j]=nodes[sel][1];
+		}
+			construct_b(arr_x,arr_y,array_b);
+			det=find_det(array_b);
+			find_inv_matrix(array_b,inv_array_b);
+			/*local_stiffness(det,inv_array_b,A_local);
+			for (k=0; k<3; k++){
+				for (l=0; l<3; l++){
+					A_global[k][l]=A_global[k][l]+A_local[k][l];
+				}
+			}*/
+	}
+
+}
 
