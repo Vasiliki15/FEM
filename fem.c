13a1de8725bccd84e458aec732154b26dfa4b223
Vasiliki Skouroliakou <vasiaskr15@gmail.com>
2020-02-05 22:43:58 +0200

    Solve the system using gsl library
    Solve the system with gsl library

--- fem.c
+++ fem.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include <gsl/gsl_math.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_permutation.h>
+#include <gsl/gsl_linalg.h>
+
+
 #define NT 24
 #define N 21
 #define d 3
@@ -13,16 +19,23 @@ enum boundary_cond {inner_node,Dirichlet};
 double find_det(const double array_b[][2]);
 void gauss_elimination(double A[][3], double vector_x[2]);
 void construct_b(double x[3], double y[3], double array_b[][2]);
-void construct_global_matrix(double A_global[][N]);
-void local_stiffness(const double array_b[][2], double det, double array_local[][3]); 
+void construct_global_matrix(double A_global[][N], double b[N]);
+void local_stiffness(const double array_b[][2], double det, double array_local[][3], double vector_local[3]); 
 void find_cord_of_nodes(int elem, double x[3], double y[3],int global_node[3]);
+gsl_matrix *construct_gsl_matrix(const double A[][N]);
+gsl_vector *construct_gsl_vector(const double b[N]);
+
+double nodes[21][2]={{-1.0,-1.0},{-0.5,-1.0},{0,-1.0},{0,-0.5},{0,0},{0.5,0},{1.0,0},{1.0,0.5},{1.0,1.0},{0.5,1.0},{0,1.0},{-0.5,1.0},{-1.0,1.0},{-1.0,0.5},{-1.0,0},{-1.0,-0.5},{-0.5,-0.5},{-0.5,0},{-0.5,0.5},{0,0.5},{0.5,0.5}};
+int connectivity[24][3] = {{2,16,1},{16,2,17},{3,17,2},{17,3,4},{17,15,16},{15,17,18},{4,18,17},{18,4,5},{19,15,18},{15,19,14},{20,18,5},{18,20,19},{12,14,19},{14,12,13},{11,19,20},{19,11,12},{6,20,5},{20,6,21},{7,21,6},{21,7,8},{21,11,20},{11,21,10},{8,10,21},{10,8,9}};
 
 int main(){
 	enum boundary_cond cond_d[21];
 	int i,j;
 	double A_global[N][N]={0};
 	double b[N]={0};
-	
+	double u[N];
+	int res;
+	FILE *fp;
 	
 /* mark nodes on the boundary,to impose Dirichlet boundary condition */
 	for (i=0; i<N; i++){
@@ -33,36 +46,38 @@ int main(){
 			cond_d[i]=inner_node;
 		}
 	}
-
+        
+	construct_global_matrix(A_global,b);
+	/*impose boundary condition*/
     for (i=0; i<N; i++){
         if (cond_d[i]==Dirichlet) {
-            b[i]=1;
+            b[i]=0;
             for (j=0; j<N; j++){
                 A_global[i][j]=0;
             }
             A_global[i][i]=1;
         }
+       
      }
-                
-   
       
-	construct_global_matrix(A_global);
-	for (i=0; i<N; i++){
-		for(j=0; j<N; j++){
-		    
-			   if ( i==j){
-			      if (A_global[i][j]==0){
-			        printf("fuck fuck fuckkkkkkkk\n");
-			       }
-			      }
-			
-		}
-	}
+   /*res=gauss_elimination_gsl(A_global,b,u);
+    if (res==0){
+        printf("System solved succesfully\n");
+    }
+    else{
+        printf("there was an error in solving the system\n");
+        return -1;
+    }
+    fp=fopen("output.dat", "w+");
+    for (j=0; j<N; j++){
+        fprintf(fp,"%lf %lf %lf\n", nodes[j][0], nodes[j][1], u[j]);
+    }
+    fclose(fp);*/
+    
 	return 0;
 }
 
-double nodes[21][2]={{-1.0,-1.0},{-0.5,-1.0},{0,-1.0},{0,-0.5},{0,0},{0.5,0},{1.0,0},{1.0,0.5},{1.0,1.0},{0.5,1.0},{0,1.0},{-0.5,1.0},{-1.0,1.0},{-1.0,0.5},{-1.0,0},{-1.0,-0.5},{-0.5,-0.5},{-0.5,0},{-0.5,0.5},{0,0.5},{0.5,0.5}};
-int connectivity[24][3] = {{2,16,1},{16,2,17},{3,17,2},{17,3,4},{17,15,16},{15,17,18},{4,18,17},{18,4,5},{19,15,18},{15,19,14},{20,18,5},{18,20,19},{12,14,19},{14,12,13},{11,19,20},{19,11,12},{6,20,5},{20,6,21},{7,21,6},{21,7,8},{21,11,20},{11,21,10},{8,10,21},{10,8,9}};
+
 
 double find_det(const double array_b[][2]){
 	return (fabs(array_b[0][0]*array_b[1][1]-array_b[1][0]*array_b[0][1]));
@@ -149,7 +164,7 @@ void gauss_elimination(double array[][3], double x[2])
   /* printf("x0=%lf, x1=%lf\n", x[0],x[1]);*/
 }
 
-void local_stiffness(const double array_b[][2], double det, double array_local[][3]){
+void local_stiffness(const double array_b[][2], double det, double array_local[][3], double vector_local[3]){
 	int i,j;
 	double area=0.5;
 	double x1[2], x2[2];
@@ -163,6 +178,7 @@ void local_stiffness(const double array_b[][2], double det, double array_local[]
 	          array_b_aug[0][2]=lambda[0][i];
 	          array_b_aug[1][2]=lambda[1][i];
 	          gauss_elimination(array_b_aug,x1);
+	          vector_local[i]=area*det*(x1[0]+x1[1]);
 	          for (j=0; j<3; j++){ 
 	                array_b_aug[0][2]=lambda[0][j];
 	                array_b_aug[1][2]=lambda[1][j];
@@ -175,17 +191,18 @@ void local_stiffness(const double array_b[][2], double det, double array_local[]
 
 }
 
-void construct_global_matrix(double A_global[][N]){
+void construct_global_matrix(double A_global[][N], double b[N]){
     int i,j,k,global_node[3];
     double x[3],y[3],det;
-    double array_b[2][2], array_local[3][3];
+    double array_b[2][2], array_local[3][3],vector_local[3];
     
    for (k=0; k<NT; k++){
         find_cord_of_nodes(k,x,y,global_node);
         construct_b(x,y,array_b);
         det=find_det(array_b);
-        local_stiffness(array_b,det,array_local);      
+        local_stiffness(array_b,det,array_local,b);      
         for (i=0; i<3; i++){
+        b[global_node[i]]=global_node[i]+vector_local[i];
             for (j=0; j<3; j++){
                      A_global[global_node[i]][global_node[j]]= A_global[global_node[i]][global_node[j]] + array_local[i][j];
              }
@@ -193,6 +210,66 @@ void construct_global_matrix(double A_global[][N]){
    }
 }
 
+gsl_matrix *construct_gsl_matrix(const double A[][N]){
+    int i,j;
+
+    gsl_matrix *gsl_m = gsl_matrix_alloc(N,N);
+    if (gsl_m == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            gsl_matrix_set(gsl_m, i, j, A[i][j]);
+        }
+    }
+
+    return gsl_m;
+}
+
+gsl_vector *construct_gsl_vector(const double b[N]){
+    int i;
+
+    gsl_vector *gsl_v = gsl_vector_alloc(N);
+    if (gsl_v == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < N; i++) {
+        gsl_vector_set(gsl_v, i, b[i]);
+    }
+
+    return gsl_v;
+}
+
+int gauss_elimination_gsl(const double A[][N], double b[N], double u[N]){
+    int s,i;
+
+     gsl_matrix *A_m = construct_gsl_matrix(A);
+     gsl_vector *b_v = construct_gsl_vector(b);
+     gsl_vector *u_v = gsl_vector_alloc(N);
+     gsl_permutation *p = gsl_permutation_alloc(N);
+
+    if (A_m == NULL || b_v == NULL || u_v == NULL || p == NULL) {
+        printf("Cannot construct matrix or/and vectors\n");
+        return -1;
+    }
+    
+    
+    /* Solve the system*/
+  
+    gsl_linalg_LU_decomp(A_m, p, &s);
+    gsl_linalg_LU_solve(A_m, p, b_v, u_v);
+    
+    printf("\nu = \n");
+    /*gsl_vector_fprintf (stdout, u_v, "%g");*/
+    
+   
+    for (i = 0; i < N; i++) {
+        u[i]=gsl_vector_get(u_v,i);
+    }
+    return 0;
+}
 
 
 
