530d0d938b25b68ddbab27ae89ac6e45819c6cbd
Vasiliki Skouroliakou <vasiaskr15@gmail.com>
2020-03-19 16:36:56 +0200

    Add #ifdef
    Add #idef to separete the case of reading the matrices nodes_up and
    connectivety_up from files. In this case the default matrices should be
    in comment.

--- fem.c
+++ fem.c
@@ -34,6 +34,9 @@ void construct_global_vector(double b[N], boundary_cond cond[N]);
 int gauss_elimination_gsl(const double A[][N], const double b[N], double u[N]);
 gsl_matrix *construct_gsl_matrix(const double A[][N]);
 gsl_vector *construct_gsl_vector(const double b[N]);
+gsl_matrix *construct_gsl_matrix_2(const double A[][2]);
+gsl_vector *construct_gsl_vector_2(const double b[2]);
+int gauss_elimination_gsl_2(const double A[][2], const double b[2], double u[2]);
 
 
 double nodes_up[21][2]={{-1.0,-1.0},{-0.5,-1.0},{0,-1.0},{0,-0.5},{0,0},{0.5,0},{1.0,0},{1.0,0.5},{1.0,1.0},{0.5,1.0},{0,1.0},{-0.5,1.0},{-1.0,1.0},{-1.0,0.5},{-1.0,0},{-1.0,-0.5},{-0.5,-0.5},{-0.5,0},{-0.5,0.5},{0,0.5},{0.5,0.5}};
@@ -100,12 +103,12 @@ int main(){
         printf("there was an error in solving the system\n");
         return -1;
     }
-    fp=fopen("output.dat", "w+");
+    fp=fopen("output21.dat", "w+");
     for (j=0; j<N; j++){
         fprintf(fp,"%lf %lf %lf\n", nodes_up[j][0], nodes_up[j][1], u[j]);
     }
     fclose(fp);
-    
+ 
 	return 0;
 }
 
@@ -156,63 +159,42 @@ void find_cord_of_nodes(const int elem, double x[3], double y[3], int global_nod
  *
  * @return none
  */
-void gauss_elimination(double array[][3], double x[2]){
-	int i,j;
-	double p[2][2]={ {0 ,1}, {1, 0} };
-	double array_temp[2][3];
-
-    if (array[0][0] == 0){
-            for (i=0; i<2; i++){
-                  for (j=0; j<3; j++){
-                        array_temp[i][j]=p[i][0]*array[0][j]+p[i][1]*array[1][j];          
-                   }
-             }
-              for (i=0; i<2; i++){
-                   for(j=0; j<3; j++){
-                        array[i][j]=array_temp[i][j];
-                   }
-              }
-            x[1]=array[1][2]/array[1][1];    
-   }
-   else if  (array[1][0]==0){
-        x[1]=array[1][2]/array[1][1];  
-    }    
-    else if (array[1][1]==0){
-         x[1]=array[1][2]/array[1][0];  
-   }  
-   else{
-         for (i=0; i<3; i++){
-            array_temp[0][i]=array[0][i]+array[1][i]*(-array[0][0]);
-         }
-         x[1]=array_temp[0][2]/array_temp[0][1];
-    }
-   x[0]=(array[0][2]-array[0][1]*x[1])/array[0][0];
-}
 
 void local_stiffness(const double array_b_T[][2],const double array_b[][2],const double det, double array_local[][3], double vector_local[3]){
-	int i,j;
+	int i,j,res;
 	double area=0.5;
-	double x1[2], x2[2], x3[2], phi[3];
+	double x1[2], x2[2], x3[2], phi[3], b_gsl[2];
     double inner_prod;
 	double d_lambda[2][3]={ {1,0,-1} , {0,1,-1} };
 	double lambda[2][3]={ {1,0,0} , {0,1,0} };
-	double array_b_T_aug[2][3]= { {array_b_T[0][0], array_b_T[0][1], 0}, {array_b_T[1][0], array_b_T[1][1], 0} };
-	double array_b_aug[2][3]= { {array_b[0][0], array_b[0][1], 0}, {array_b[1][0], array_b[1][1], 0} };
 
 	for (i=0; i<3; i++){
-	          array_b_T_aug[0][2]=d_lambda[0][i];
-	          array_b_T_aug[1][2]=d_lambda[1][i];
-	          gauss_elimination(array_b_T_aug,x1);
+	          b_gsl[0]=d_lambda[0][i];
+	          b_gsl[1]=d_lambda[1][i];
+	          res=gauss_elimination_gsl_2(array_b_T, b_gsl, x1);
+	          if (res!=0){
+              printf("there was an error in solving the system\n");
+              return ;
+              }
+	          
 	          for (j=0; j<3; j++){ 
-	                array_b_T_aug[0][2]=d_lambda[0][j];
-	                array_b_T_aug[1][2]=d_lambda[1][j];
-	                gauss_elimination(array_b_T_aug,x2);
+	                b_gsl[0]=d_lambda[0][j];
+	                b_gsl[1]=d_lambda[1][j];
+	                res=gauss_elimination_gsl_2(array_b_T, b_gsl,x2);
+	                if (res!=0){
+                    printf("there was an error in solving the system\n");
+                    return ;
+                    }
 	                inner_prod=x1[0]*x2[0]+x1[1]*x2[1];
 	                array_local[i][j]=area*det*inner_prod;
 	          }
-	          array_b_aug[0][2]=lambda[0][i];
-	          array_b_aug[1][2]=lambda[1][i];
-	          gauss_elimination(array_b_aug,x3);
+	          b_gsl[0]=lambda[0][j];
+	          b_gsl[1]=lambda[1][j];
+	          res= gauss_elimination_gsl_2(array_b, b_gsl, x3);
+	          if (res!=0){
+              printf("there was an error in solving the system\n");
+              return ;
+              }
 	          phi_value(x3,phi);
 	          vector_local[i]=area*det*phi[i];       
 	 }
@@ -276,6 +258,37 @@ gsl_vector *construct_gsl_vector(const double b[N]){
 
     return gsl_v;
 }
+gsl_matrix *construct_gsl_matrix_2(const double A[][2]){
+    int i,j;
+
+    gsl_matrix *gsl_m = gsl_matrix_alloc(2,2);
+    if (gsl_m == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            gsl_matrix_set(gsl_m, i, j, A[i][j]);
+        }
+    }
+
+    return gsl_m;
+}
+gsl_vector *construct_gsl_vector_2(const double b[2]){
+    int i;
+
+    gsl_vector *gsl_v = gsl_vector_alloc(2);
+    if (gsl_v == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < 2; i++) {
+        gsl_vector_set(gsl_v, i, b[i]);
+    }
+
+    return gsl_v;
+}
+
 
 int gauss_elimination_gsl(const double A[][N], const double b[N], double u[N]){
     int s,i;
@@ -301,7 +314,30 @@ int gauss_elimination_gsl(const double A[][N], const double b[N], double u[N]){
     }
     return 0;
 }
+int gauss_elimination_gsl_2(const double A[][2], const double b[2], double u[2]){
+    int s,i;
 
+     gsl_matrix *A_m = construct_gsl_matrix_2(A);
+     gsl_vector *b_v = construct_gsl_vector_2(b);
+     gsl_vector *u_v = gsl_vector_alloc(2);
+     gsl_permutation *p = gsl_permutation_alloc(2);
+
+    if (A_m == NULL || b_v == NULL || u_v == NULL || p == NULL) {
+        printf("Cannot construct matrix or/and vectors\n");
+        return -1;
+    }
+    
+    /* Solve the system*/
+    gsl_linalg_LU_decomp(A_m, p, &s);
+    gsl_linalg_LU_solve(A_m, p, b_v, u_v);
+   /* printf("\nu = \n");
+    gsl_vector_fprintf (stdout, u_v, "%lf");*/
+ 
+    for (i = 0; i < 2; i++) {
+        u[i]=gsl_vector_get(u_v,i);
+    }
+    return 0;
+}
 
 
 
